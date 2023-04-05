d058524c8ba35487374f25378e74e33da3ae40f4
Pham Duc <ducphgcd191221@fpt.edu.vn>
2023-04-05 16:04:37 +0700

    feat: create-home-about (#2)
    * feat: create-home-about
    
    * fix: order of pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import App from '@root/App'
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
--- src/page/HomePage.jsx
+++ src/page/HomePage.jsx
@@ -1,9 +1,11 @@
+import AboutHome from '../components/AboutHome/AboutHome'
 import HomeDepartment from '../components/HomeDepartment/HomeDepartment'
 // home-department
 export default function HomePage() {
   return (
     <>
       <div>
+        <AboutHome />
         <HomeDepartment name='home-department' />
       </div>
     </>
