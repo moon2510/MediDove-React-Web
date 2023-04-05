c5c1fd4969dd9e6c76e1715768daebc3f029c10b
Bui Thi Minh Nguyet <62028865+moon2510@users.noreply.github.com>
2023-04-05 15:15:26 +0700

    feat: create-home-department (#1)

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import App from '@root/App'
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
-import '@root/styles/index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
