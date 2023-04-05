ab13600eb523ca7f6768403e629f97ac241a55a4
moon2510 <nguyetminhthibui@gmail.com>
2023-04-05 16:07:41 +0700

    feat: create-department-top-part

---
+++ src/components/BannerDepartment/BannerDepartment.jsx
@@ -0,0 +1,7 @@
+import './BannerDepartment.scss'
+
+// import { Link } from 'react-router-dom'
+
+export default function BannerDepartment(bannerStyle) {
+  return <section className={bannerStyle.name}></section>
+}
