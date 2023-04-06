7fa0d58304d3d91fba4062a0cb796de2235e1203
ltkien <ltkienwork@gmail.com>
2023-04-06 10:48:53 +0700

    feat: create-home-banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
-import Example from '@components/Example/Example'
+import HomeBanner from './components/Banner/HomeBanner'
+import { useEffect } from 'react'
 export default function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1100,
+      delay: 400,
+    })
+  }, [])
   return (
     <>
-      <Example />
+      <HomeBanner />
     </>
   )
 }
