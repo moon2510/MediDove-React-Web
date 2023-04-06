7266bc4abfacdb25423c0e8af74767223da8918e
ltkien <ltkienwork@gmail.com>
2023-04-06 22:36:57 +0700

    feat: Submit code

---
+++ src/page/ServicesDetailsPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ServicesDetailsArea from '../components/ServicesDetails/ServicesDetailsArea/ServicesDetailsArea'
+
+export default function ServicesDetailsPage() {
+  return (
+    <div>
+      <ServicesDetailsArea />
+    </div>
+  )
+}
