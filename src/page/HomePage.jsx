28b3db80c0b1eea520ebbe870fe1567bef2c38ac
kiranguyen2000 <98039366+kiranguyen2000@users.noreply.github.com>
2023-04-06 10:25:23 +0700

    feat: create-home-our-team (#6)
    * feat: create-home-our-team
    
    * fix: change-directory-router-homepage
    
    * fix: add-router-for-home-page
    
    * fix: fix-css-home-our-team

--- src/page/HomePage.jsx
+++ src/page/HomePage.jsx
@@ -1,5 +1,6 @@
 import AboutHome from '../components/AboutHome/AboutHome'
 import HomeDepartment from '../components/HomeDepartment/HomeDepartment'
+import HomeOurTeam from '../components/HomeOurTeam/HomeOurTeam'
 // home-department
 export default function HomePage() {
   return (
@@ -7,6 +8,7 @@ export default function HomePage() {
       <div>
         <AboutHome />
         <HomeDepartment name='department-service' />
+        <HomeOurTeam name='home-our-team' />
       </div>
     </>
   )
