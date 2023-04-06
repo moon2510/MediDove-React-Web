8c2598d4011703a499646054611921231b72fe54
ltkien <ltkienwork@gmail.com>
2023-04-06 14:41:49 +0700

    feat: create-doctor-detail-(demo)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import DoctorDetails from './components/Doctor/DoctorDetails/DoctorDetails'
 
 import DepartmentPage from './page/DepartmentPage'
 import HomePage from './page/HomePage'
@@ -9,6 +10,7 @@ export default function App() {
         <Routes>
           <Route element={<HomePage />} path='/' />
           <Route element={<DepartmentPage />} path='/services' />
+          <Route element={<DoctorDetails />} path='/doctorDetails' />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/components/Doctor/DoctorDetails/DoctorDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import DoctorDetailsArea from './DoctorDetailsArea/DoctorDetailsArea'
+
+const DoctorDetails = () => {
+  return (
+    <>
+      <DoctorDetailsArea />
+    </>
+  )
+}
+
+export default DoctorDetails
