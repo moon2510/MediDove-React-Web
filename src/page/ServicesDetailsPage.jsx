ea5e3c724370d29212cf33abf82357fedf71ac79
ltkien <ltkienwork@gmail.com>
2023-04-07 15:20:02 +0700

    feat: Add header,footer

--- src/page/ServicesDetailsPage.jsx
+++ src/page/ServicesDetailsPage.jsx
@@ -1,10 +1,14 @@
+import DoctorFooter from '../components/DoctorFooter/DoctorFooter'
+import Header from '../components/Header/Header'
 import React from 'react'
 import ServicesDetailsArea from '../components/ServicesDetails/ServicesDetailsArea/ServicesDetailsArea'
 
 export default function ServicesDetailsPage() {
   return (
     <div>
+      <Header />
       <ServicesDetailsArea />
+      <DoctorFooter />
     </div>
   )
 }
