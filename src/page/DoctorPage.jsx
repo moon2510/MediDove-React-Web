0d7444d089660f99db10414b1e15859817e5cb3a
kiranguyen2000 <98039366+kiranguyen2000@users.noreply.github.com>
2023-04-07 12:09:10 +0700

    feat: create-doctor-counter (#17)
    * feat: create-doctor-counter
    
    * fix: import-aos-app.jsx

--- src/page/DoctorPage.jsx
+++ src/page/DoctorPage.jsx
@@ -1,4 +1,5 @@
 import BannerDepartment from '../components/BannerDepartment/BannerDepartment'
+import CounterArea from '../components/CounterArea/CounterArea'
 import Available from '../components/Doctor/DoctorOne/Available/Available'
 
 // home-department
@@ -8,6 +9,7 @@ export default function DoctorPage() {
       <div>
         <BannerDepartment subtitle={'Doctor'} title={'Doctor 01'} />
         <Available />
+        <CounterArea />
       </div>
     </>
   )
