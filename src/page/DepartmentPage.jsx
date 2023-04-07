78dfa5cf6ffe2593fd5557b7d4752da59043fcaa
nguyendeptrai <nguyendeptrai@gmail.com>
2023-04-07 13:05:13 +0700

    fix: department-bottom-part

--- src/page/DepartmentPage.jsx
+++ src/page/DepartmentPage.jsx
@@ -3,7 +3,6 @@ import DoctorFooter from '../components/DoctorFooter/DoctorFooter'
 import Header from '../components/Header/Header'
 import HomeDepartment from '../components/HomeDepartment/HomeDepartment'
 import DepartmentBottom from '../components/DepartmentBottom/DepartmentBottom'
-import ForEmployers from '../components/For Employers/ForEmployers'
 
 export default function DepartmentPage() {
   return (
