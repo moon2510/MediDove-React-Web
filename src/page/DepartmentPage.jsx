85251b7174ac145332bed7d9b123650786e1a92a
unknown <duongtrankhoinguyen1207@gmail.com>
2023-04-07 12:16:51 +0700

    feat: create-department-bottom-part

--- src/page/DepartmentPage.jsx
+++ src/page/DepartmentPage.jsx
@@ -1,5 +1,7 @@
 import BannerDepartment from '../components/BannerDepartment/BannerDepartment'
 import HomeDepartment from '../components/HomeDepartment/HomeDepartment'
+import DepartmentBottom from '../components/DepartmentBottom/DepartmentBottom'
+import ForEmployers from '../components/For Employers/ForEmployers'
 
 export default function DepartmentPage() {
   return (
@@ -7,6 +9,8 @@ export default function DepartmentPage() {
       <div>
         <BannerDepartment subtitle={'Service'} title={'Our Services'} />
         <HomeDepartment name='department-service' />
+        <DepartmentBottom />
+        <ForEmployers />
       </div>
     </>
   )
